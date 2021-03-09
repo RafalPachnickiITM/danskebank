77be28ba353842c7f5e5262356fc8c2c49887727
Rafał Pachnicki <rafal.pachnicki@itmagination.com>
2021-03-09 22:44:55 +0100

    Initialization project

---
+++ danskebank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace danskebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
